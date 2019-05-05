5f5046294007c7f82c2000226cb9b6b1f7faf0a1
אסף מירום < @DESKTOP-MLFMRRI>
2019-05-05 15:27:35 +0300

    First Change

---
+++ MusicStore/MusicStore/Models/RecordLabel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class RecordLabel
+    {
+        public int Id { get; set; }
+        public String name { get; set; }
+        public Artists { get; set; }
+        public int Id { get; set; }
+        public int Id { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ MusicStore/MusicStore/Models/Song.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public String title { get; set; }
+        public String artist { get; set; }
+        public DateTime releaseDate { get; set; }
+        public DateTime upLoadDate { get; set; }
+        public String language { get; set; }
+        public int likes { get; set; }
+
+        public TimeSpan length { get; set; }
+
+        public String bitRate { get; set; }
+        public String album { get; set; }
+
+        public int trackNumber { get; set; }
+        public String genre { get; set; }
+        public Decimal price { get; set; }
+
+
+
+
+
+
+
+    }
+}
